cb3b03c0cd705805fb1c907e4903908bb85c3482
Pranat <pranat@lucknowmarbles.com>
2022-01-19 13:39:10 +0530

    first Commit

---
+++ models/product.js
@@ -0,0 +1,20 @@
+const { intersection } = require('lodash');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    minPrice: Number,
+    maxPrice: Number,
+    thickness: Number,
+    name: String,
+    unit: String,
+    color: String,
+    image: {
+        data: Buffer,
+        contentType: String
+    },
+    productType: String,
+    placeOfOrigin: String
+});
+
+module.exports = mongoose.model('Product', productSchema);
---
+++ models/saleCutEntry.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const saleCutEntrySchema = new Schema({
+    
+    quantity: Number,
+    pieceId: String,
+    saleId: String
+    
+});
+
+module.exports = mongoose.model('SaleCutEntry', saleCutEntrySchema);
+ 
